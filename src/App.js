3c588ef077b2761d389ff4c11cdc48ca31f75947
Lexington Chaparro <66759495+lexx54@users.noreply.github.com>
2021-03-28 13:06:15 -0400

    added functions to add new Names

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
+import React, { useState } from "react";
+import Contact from "./Components/Contact";
 import "./styles.css";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
 
+  //functions
+  const setContactName = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+  const addNewContact = (e) => {
+    e.preventDefault();
+    const contactData = {
+      name: newName,
+      id: persons.length + 1
+    };
+    setPersons(persons.concat(contactData));
+    setNewName("");
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewContact}>
         <div>
-          name: <input />
+          name:{" "}
+          <input
+            value={newName}
+            placeholder="Insert Your name"
+            onChange={setContactName}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul>
+          {persons.map((person) => (
+            <Contact data={person} key={person.id} />
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = ({ data }) => {
+  return <li>{data.name}</li>;
+};
+
+export default Contact;
